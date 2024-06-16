3951f7082e0a4e5177fc792c6113b07a0c31f723
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2024-06-16 09:01:56 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -3,29 +3,46 @@ let variable = "Hello 34"
 let id2 = undefined
 let id3 = true
 
-console.log(typeof id );
-console.log(typeof variable);
-console.log(typeof (id3));
+// console.log(typeof id );
+// console.log(typeof variable);
+// console.log(typeof (id3));
 
 let valueofid = Number(id)
 let valueofvar = Number(variable)
 let valueofid2 =Number(id3)
 
 
-console.log(typeof valueofid);
-console.log(typeof valueofvar);
-console.log(typeof valueofid3)
+// console.log(typeof valueofid);
+// console.log(typeof valueofvar);
+// console.log(typeof valueofid3)
 
 
-console.log(valueofid);
-console.log(valueofvar);
+// console.log(valueofid);
+// console.log(valueofvar);
 
 
 // 0 or "" => false
 // 1 or "gd" => true
 
 let boolvariable = Boolean(variable)
-console.log( boolvariable);
+// console.log( boolvariable);
 
 let stringid = String(id)
-console.log(stringid);
+// console.log(stringid);
+
+
+// ******************OPERATIONs**********************
+
+console.log(1+3+"3")
+// o/p => 43
+
+// vs
+
+console.log("1"+3+3);
+// o/p => 133
+
+// NOT RECOMMENDED TO USE (^_^)
+// NOT A NICE WAY 
+
+console.log(true)
+console.log(+true);
