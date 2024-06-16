b7df6770b59b19b8a7c697ec6b215721d1e71ce8
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2024-06-16 05:14:33 +0000

    variables and datatypes.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+// constant variable
+const id = 3825
+// variable
+let name = "Robin"
+
+console.log(id);
+// update the name 
+name = "Steve"
+/*
+now printing the new name and id
+*/
+console.table([id,name])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; // for latest version of JS
+let name = "Robin"
+let age = 20
+let bool = false
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof bool);
