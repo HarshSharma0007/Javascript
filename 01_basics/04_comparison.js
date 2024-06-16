de7f27cd59795d035f6aaf5ef29509970baf56b1
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2024-06-16 09:29:22 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// basics
+
+console.log(1==1)
+console.log(1>=1)
+console.log(1<=1)
+console.log(1!=1)
+console.log(1>1)
+console.log(1<1)
+
+
+
+// ***********Problems in JS ***************
+
+console.log("02">1)
+
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+
+//   ******** for strict checking of numbers ************* 
+
+console.log("1"===1);
+console.log(1===1);
