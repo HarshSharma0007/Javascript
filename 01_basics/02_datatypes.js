d4319e1f54da1c5fcf3d5f6ae638e75448ea3352
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-06-07 17:48:45 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,8 +1,14 @@
-"use strict"; // for latest version of JS
+"use strict"; // for treating latest version of JS
+
+// alert( 3+3) // since it will only work in browser
+
+
+
 let name = "Robin"
 let age = 20
 let bool = false
-
+// null is an stand alone value
+// undefined when value is not defined
 console.log(typeof name);
 console.log(typeof age);
 console.log(typeof bool);
