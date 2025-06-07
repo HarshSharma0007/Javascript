38f1711ed0e3816deb3c9d4f28216435b5c5dc6c
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-06-07 18:21:46 +0000

    datatype summary

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -9,15 +9,15 @@ console.log(1<1)
 
 
 
-// ***********Problems in JS ***************
-
+// *********** Problems in JS ***************
+// AVOID AVOID AVOID AVOID AVOID AVOID AVOID AVOID
 console.log("02">1)
 
-console.log(null >= 0);
-console.log(null == 0);
+console.log(null >= 0); // comparisons
+console.log(null == 0); // equality check
 console.log(null > 0);
 
 //   ******** for strict checking of numbers ************* 
-
+// checks even for datatype
 console.log("1"===1);
 console.log(1===1);
--- 01_basics/05_datatype-summary.js
+++ 01_basics/05_datatype-summary.js
@@ -1,1 +1,35 @@
 // Primitive Datatype : Boolean , Number , String ,BigInt, Symbol, null , undefined
+
+const score = 100
+
+const scoreValur = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userMail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+// Arrays
+
+const hero = ["h1", "h2", "h3"]
+
+let myObj = {
+    name : "myname",
+    age : 19,
+}
+
+const myFunction = function(){
+    console.log("Hello");
+    
+}
+console.log(typeof null);
+console.log(typeof myObj);
+console.log(typeof id);
+console.log(typeof hero);
+
+
+console.log(typeof myFunction)
